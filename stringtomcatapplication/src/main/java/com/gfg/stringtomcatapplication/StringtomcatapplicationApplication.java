8236fe28c3f204b76ff80f38e971ee837766eac5
Ubuntu <ubuntu@ip-172-31-20-95.us-east-2.compute.internal>
2024-10-11 13:43:57 +0000

    Initial commit or your commit message

---
+++ stringtomcatapplication/src/main/java/com/gfg/stringtomcatapplication/StringtomcatapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.gfg.stringtomcatapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringtomcatapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringtomcatapplicationApplication.class, args);
+	}
+
+}
---
+++ stringtomcatapplication/src/test/java/com/gfg/stringtomcatapplication/StringtomcatapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfg.stringtomcatapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringtomcatapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
